0dd3fdcf8a9b39ce2eb5c4f8436055456e9b76da
Robert Elia <relia@pdx.edu>
2024-03-13 11:13:37 -0700

    Adding a2d comments

--- src/a2d.rs
+++ src/a2d.rs
@@ -1,13 +1,44 @@
+/// SAADC - Successive Approximation Analog-toDigital Converter
+/// Using SAADC configured with analog inputs, take measurements
+/// and convert those to a digital representation
 use crate::*;
 
+/// A type alias for an Saadc object with 2 channels
 pub type Adc = saadc::Saadc<'static, 2>;
 
+/// This datatype calibrates the ADC and provides 2 async functions
+/// 1. measure_knob(): This gets a sample back from our potentometer
+/// 2. measure_ldr():  This gets a sample back from our photoresistor
 pub struct A2d(Adc);
 impl A2d {
+    /// Create a new 'A2d' and calibrates the ACD
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// let mut board = embassy_nrf::init(Default::default());
+    /// let mut saadc_config = saadc::Config::default();
+    /// saadc_config.resolution = saadc::Resolution::_14BIT;
+    /// let saadc = saadc::Saadc::new(
+    ///    board.SAADC,
+    ///    Irqs,
+    ///    saadc_config,
+    ///    [
+    ///        saadc::ChannelConfig::single_ended(&mut board.P0_04), // P2
+    ///        saadc::ChannelConfig::single_ended(&mut board.P0_28), // P4
+    ///    ],
+    /// );
+    ///
+    /// let a2d = A2d::new(saadc).await;
+    ///
+    /// a2d.measure_knob();
+    /// ```
     pub async fn new(adc: Adc) -> Self {
         adc.calibrate().await;
         Self(adc)
     }
+
+    /// Return a sample from the potentometer and scale it between 0 and 15
 
     pub async fn measure_knob(&mut self) -> u32 {
         let mut buf = [0, 0];
@@ -20,55 +51,11 @@ impl A2d {
         result as u32
     }
 
+    /// Return a sample from the ldr
+    /// TODO add scaling
     pub async fn measure_ldr(&mut self) -> u32 {
         let mut buf = [0, 0];
         self.0.sample(&mut buf).await;
         buf[1] as u32
     }
 }
-/*
-pub struct Knob<'a> {
-    saadc: &'a mut Adc2,
-}
-
-impl<'a> Knob<'a> {
-    pub async fn new(adc: &'a mut Adc2) -> Self {
-        adc.calibrate().await;
-        Self { saadc: adc }
-    }
-
-    pub async fn measure(&mut self) -> u32 {
-        let mut buf = [0, 0];
-        self.saadc.sample(&mut buf).await;
-        let raw = buf[0].clamp(0, 0x7fff) as u16;
-        let scaled = raw as f32 / 10_000.0;
-        let result = ((LEVELS + 2) as f32 * scaled - 2.0)
-            .clamp(0.0, (LEVELS - 1) as f32)
-            .floor();
-        result as u32
-    }
-}
-
-/// Light dependent resistor
-pub struct Ldr<'a> {
-    saadc: &'a mut Adc2,
-}
-
-impl<'a> Ldr<'a> {
-    pub async fn new(adc: &'a mut Adc2) -> Self {
-        adc.calibrate().await;
-        Self { saadc: adc }
-    }
-
-    pub async fn measure(&mut self) -> u32 {
-        let mut buf = [0, 0];
-        self.0.sample(&mut buf).await;
-        let raw = buf[0].clamp(0, 0x7fff) as u16;
-        let scaled = raw as f32 / 10_000.0;
-        let result = ((LEVELS + 2) as f32 * scaled - 2.0)
-            .clamp(0.0, (LEVELS - 1) as f32)
-            .floor();
-        result as u32
-    }
-}
-*/
