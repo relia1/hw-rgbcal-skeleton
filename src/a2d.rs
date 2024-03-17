dd4fa948df3a9006557f651813cf487d8ecd0695
Robert Elia <relia@pdx.edu>
2024-03-17 00:42:06 -0700

    updating scaling for knob

--- src/a2d.rs
+++ src/a2d.rs
@@ -39,15 +39,20 @@ impl A2d {
     }
 
     /// Return a sample from the potentiometer and scale it between 0 and 15
+    /// Digital Output Result = (V(P) – V(N)) * (GAIN/REFERENCE) * (2^(RESOLUTION - m))
+    /// V(P) is the voltage at input P
+    /// V(N) is the voltage at input N
+    /// GAIN is the selected gain
+    /// REFERENCE is the selected reference voltage
+    /// RESOLUTION is output resolution in bits
+    /// m is 0 for single ended channels and 1 for differential channels
     pub async fn measure_knob(&mut self) -> u32 {
         let mut buf = [0, 0];
         self.0.sample(&mut buf).await;
         let raw = buf[0].clamp(0, 0x7fff) as u16;
-        let scaled = raw as f32 / 10_000.0;
-        let result = ((LEVELS + 2) as f32 * scaled - 2.0)
-            .clamp(0.0, (LEVELS - 1) as f32)
-            .floor();
-        result as u32
+        // Ratio for scaling to 0 to 15 inclusive of 15
+        let ratio = (raw as f32) / 15_000_f32;
+        (ratio * 16_f32) as u32
     }
 
     /// Return a sample from the ldr
